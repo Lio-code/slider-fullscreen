1d19ddf47d0f7d9164865ea1608f334f543ca62f
Lionel <lio4455@hotmail.com>
2022-03-18 16:44:04 +0100

    finish

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
+import { SliderData } from './SliderData';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
 const ImageSlider = ({ slides }) => {
   const [current, setCurrent] = useState(0);
@@ -13,8 +14,6 @@ const ImageSlider = ({ slides }) => {
     setCurrent(current === 0 ? length - 1 : current - 1);
   };
 
-  console.log(current);
-
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
@@ -23,15 +22,16 @@ const ImageSlider = ({ slides }) => {
     <section className="slider">
       <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
       <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
-
-      {slides.map((slide, index) => {
+      {SliderData.map((slide, index) => {
         return (
-          <img
+          <div
+            className={index === current ? 'slide active' : 'slide'}
             key={index}
-            src={slide.image}
-            alt="random images"
-            className="image"
-          />
+          >
+            {index === current && (
+              <img src={slide.image} alt="random img" className="image" />
+            )}
+          </div>
         );
       })}
     </section>
